10e9a412d4592c83ef87edcdf9f9e51519351962
mansi0321 <70951280+mansi0321@users.noreply.github.com>
2021-06-29 17:27:14 +0530

    Add files via upload

---
+++ Insurance/app/src/main/java/sumit/mansi/insurance/MainActivity.java
@@ -0,0 +1,14 @@
+package sumit.mansi.insurance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
